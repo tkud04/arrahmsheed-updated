bac580cad8f9406da79eee947db7f4474388aced
tkud04 <32499735+tkud04@users.noreply.github.com>
2018-06-09 05:21:39 +0100

    Update msg

--- resources/views/emails/msg.blade.php
+++ resources/views/emails/msg.blade.php
@@ -1,5 +1,14 @@
-<h3>{{$name}}: {{$subject}}</h3><br>
+<?php
+print_r($name); echo "<br>";
+print_r($subject); echo "<br>";
+print_r($mail); echo "<br>";
+print_r($message); echo "<br>";
+
+/*<h3>{{$name}}: {{$subject}}</h3><br>
 <p>Hi, you've got a new message from {{$email}}</p>
 <div style="margin: 5px;">
 {{$message}} 
-</div>
+</div>*/
+
+?>
+
